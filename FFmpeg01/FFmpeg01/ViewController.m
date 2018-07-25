a7235b0a14eb4872966e1076e036cfdfb74ace4e
Cstzhangzb <devzhangzb@163.com>
2018-07-25 16:42:35 +0800

    测试打开文件

--- FFmpeg01/FFmpeg01/ViewController.m
+++ FFmpeg01/FFmpeg01/ViewController.m
@@ -18,6 +18,8 @@
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
     [FFmpegTest ffmpegTestConfig];
+    NSString *path =  [[NSBundle mainBundle] pathForResource:@"cat" ofType:@".mp4"];
+    [FFmpegTest ffmpegOpenFile:path];
 }
 
 
