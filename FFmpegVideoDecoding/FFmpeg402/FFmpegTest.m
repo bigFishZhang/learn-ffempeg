0de6441472cefbb7fa87404e0089c86b7a2ca9f3
Cstzhangzb <devzhangzb@163.com>
2018-08-01 16:04:43 +0800

    av_read_frame

--- FFmpegVideoDecoding/FFmpeg402/FFmpegTest.m
+++ FFmpegVideoDecoding/FFmpeg402/FFmpegTest.m
@@ -31,7 +31,7 @@ static double r2d(AVRational r){
     
     // 2 打开封装格式文件
     //封装格式上下文
-    AVFormatContext *avformat_context = avformat_alloc_context();
+    AVFormatContext *ic = avformat_alloc_context();
     
     //文件路径
     const char *url = [filePath UTF8String];
@@ -42,31 +42,31 @@ static double r2d(AVRational r){
      • AVInputFormat *fmt 指定输入的封装格式 一般情况传NULL即可，自行判断即可
      • AVDictionary **options 一般传NULL
      */
-    int avformat_open_input_result = avformat_open_input(&avformat_context, url, NULL, NULL);
+    int re = avformat_open_input(&ic, url, NULL, NULL);
     
-    if (avformat_open_input_result != 0) {
+    if (re != 0) {
         NSLog(@"打开文件失败");
         char *error_info = NULL;
-        av_strerror(avformat_open_input_result, error_info, 1024);
+        av_strerror(re, error_info, 1024);
         
         return;
     }
     
     NSLog(@"打开文件成功");
     //获取流信息 读取部分视频做探测
-    avformat_open_input_result = avformat_find_stream_info(avformat_context, 0);
-    if (avformat_open_input_result != 0) {
+    re = avformat_find_stream_info(ic, 0);
+    if (re != 0) {
         NSLog(@"avformat_find_stream_info failed!");
     }
     //总时长，流的信息
-    NSLog(@"duration =%lld nb_streams = %d",avformat_context->duration,avformat_context->nb_streams);
+    NSLog(@"duration =%lld nb_streams = %d",ic->duration,ic->nb_streams);
     //打印相关信息
     int fps = 0;
     int videoStream = 0;
     int audioStream = 1;
     //(1)遍历
-    for (int i = 0; i < avformat_context->nb_streams; i++) {
-        AVStream *as = avformat_context->streams[i];
+    for (int i = 0; i < ic->nb_streams; i++) {
+        AVStream *as = ic->streams[i];
         if (as->codecpar->codec_type == AVMEDIA_TYPE_VIDEO) {
             NSLog(@"VIDEO");
             videoStream = i;
@@ -101,11 +101,31 @@ static double r2d(AVRational r){
      @param flags#> 暂时无用 description#>
      @return <#return value description#>
      */
-    audioStream = av_find_best_stream(avformat_context, AVMEDIA_TYPE_AUDIO, -1, -1, NULL, 0);
+    audioStream = av_find_best_stream(ic, AVMEDIA_TYPE_AUDIO, -1, -1, NULL, 0);
     NSLog(@" av_find_best_stream : %d ",audioStream);
     
+    //读取帧数据
+    AVPacket *pkt = av_packet_alloc();//创建一个对象空间，初始化
+    for (; ; ) {//无线循环
+        int re = av_read_frame(ic, pkt);
+        if (re != 0) {
+            NSLog(@"读取到结尾处！");
+            int pos = 3 * r2d(ic->streams[videoStream]->time_base);;
+            av_seek_frame(ic, videoStream, pos, AVSEEK_FLAG_BACKWARD|AVSEEK_FLAG_FRAME);
+            continue;
+        }
+        NSLog(@"stream = %d size = %d pts = %lld flag = %d",pkt->stream_index,
+              pkt->size,pkt->pts,pkt->flags);
+        /////操作
+        
+        
+        av_packet_unref(pkt);
+    }
+
     //关闭输入的上下文 释放内存
-    avformat_close_input(&avformat_context);
+    avformat_close_input(&ic);
+    
+    
 }
 
 
