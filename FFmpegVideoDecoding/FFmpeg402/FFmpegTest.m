f5afbb2a2a20b67d772e86cf9d49d028c5462af9
Cstzhangzb <devzhangzb@163.com>
2018-08-02 14:31:51 +0800

    FFmpeg 解码

--- FFmpegVideoDecoding/FFmpeg402/FFmpegTest.m
+++ FFmpegVideoDecoding/FFmpeg402/FFmpegTest.m
@@ -29,7 +29,10 @@ static double r2d(AVRational r){
     // 2 初始化网络 如果需要的话
     avformat_network_init();
     
-    // 2 打开封装格式文件
+    // 初始化 解码器
+//    avcodec_register_all();
+    
+    // 3 打开封装格式文件
     //封装格式上下文
     AVFormatContext *ic = avformat_alloc_context();
     
@@ -104,21 +107,93 @@ static double r2d(AVRational r){
     audioStream = av_find_best_stream(ic, AVMEDIA_TYPE_AUDIO, -1, -1, NULL, 0);
     NSLog(@" av_find_best_stream : %d ",audioStream);
     
+    //软解码器
+    AVCodec *codec = avcodec_find_decoder(ic->streams[videoStream]->codecpar->codec_id);
+    //硬解码
+//    codec = avcodec_find_decoder_by_name("h264_mediacodec");
+    if (!codec) {
+        NSLog(@"avcodec_find_decoder_by_name  video failed!");
+        return ;
+    }
+    //解码器初始化
+    AVCodecContext *vc = avcodec_alloc_context3(codec);
+    vc->thread_count = 1;
+    avcodec_parameters_to_context(vc, ic->streams[videoStream]->codecpar);
+    //打开解码器
+    re = avcodec_open2(vc,0, 0);
+    if (re != 0) {
+         NSLog(@"avcodec_open2 video failed!");
+         return;
+    }
+    
+    ////////////////////////////////////////////////////
+    //音频解码器
+    AVCodec *acodec = avcodec_find_decoder(ic->streams[audioStream]->codecpar->codec_id);
+    //硬解码
+    //    codec = avcodec_find_decoder_by_name("h264_mediacodec");
+    if (!acodec) {
+        NSLog(@"avcodec_find_decoder_by_name audio failed!");
+        return ;
+    }
+    //解码器初始化
+    AVCodecContext *ac = avcodec_alloc_context3(acodec);
+    ac->thread_count = 1;
+    avcodec_parameters_to_context(ac, ic->streams[audioStream]->codecpar);
+    //打开解码器
+    re = avcodec_open2(ac,0, 0);
+    if (re != 0) {
+        NSLog(@"avcodec_open2 audio failed!");
+        return;
+    }
+    
     //读取帧数据
     AVPacket *pkt = av_packet_alloc();//创建一个对象空间，初始化
+    AVFrame *frame = av_frame_alloc();
     for (; ; ) {//无线循环
         int re = av_read_frame(ic, pkt);
+  
+        
         if (re != 0) {
             NSLog(@"读取到结尾处！");
             int pos = 3 * r2d(ic->streams[videoStream]->time_base);;
             av_seek_frame(ic, videoStream, pos, AVSEEK_FLAG_BACKWARD|AVSEEK_FLAG_FRAME);
             continue;
         }
-        NSLog(@"stream = %d size = %d pts = %lld flag = %d",pkt->stream_index,
-              pkt->size,pkt->pts,pkt->flags);
+        
+        //测试音视频解码
+        AVCodecContext *cc = vc;
+        if (pkt->stream_index == audioStream) {
+            cc = ac;
+        }
+        
+        //发送的线程中解码
+        re = avcodec_send_packet(cc, pkt);\
+        //解码
+        av_packet_unref(pkt);
+        
+        if (re != 0) {
+            NSLog(@"avcodec_send_packet audio failed!");
+            continue;
+        }
+        for (; ; ) {//保证能搜到全部的解码数据
+            re = avcodec_receive_frame(cc, frame);
+            if (re != 0) {
+//                NSLog(@"avcodec_receive_frame audio failed!");
+                break;
+            }
+            NSLog(@"frame->pts %lld",frame->pts);
+        }
+        //最后一针的时候 avcodec_send_packet(cc, NULL)
+        
+        
+    
+        
+        
+        
+//        NSLog(@"stream = %d size = %d pts = %lld flag = %d",pkt->stream_index,
+//              pkt->size,pkt->pts,pkt->flags);
         /////操作
-        
-        
+
         av_packet_unref(pkt);
     }
 
