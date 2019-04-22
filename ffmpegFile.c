2ec98c0631ec17239bfba45741abe1b4ff8388bc
Cstzhangzb <devzhangzb@163.com>
2019-04-22 14:04:41 +0800

    re name file

--- ffmpegFile.c
+++ ffmpegFile.c
@@ -8,15 +8,29 @@ void creatTestFile()
   fwrite("hello", 1, 5, file);
   fclose(file);
 }
+int changeName(char *newNme)
+{
+  int ret;
+  ret = avpriv_io_move("mytest.txt", newNme);
+  return ret;
+}
 
 int main(int argc, char *argv[])
 {
-  int ret;
+  int ret, re;
   creatTestFile();
-  ret = avpriv_io_delete("./mytest.txt");
+  re = changeName("newText.txt");
+  if (re < 0)
+  {
+    av_log(NULL, AV_LOG_ERROR, "Failed to reName file mytest.txt \n");
+    return re;
+  }
+  av_log(NULL, AV_LOG_INFO, "Success to reName file mytest.txt \n");
+
+  ret = avpriv_io_delete("./newText.txt");
   if (ret < 0)
   {
-    av_log(NULL, AV_LOG_ERROR, "Failed to delete file mytest.txt \n");
+    av_log(NULL, AV_LOG_ERROR, "Failed to delete file newText.txt \n");
     return ret;
   }
   av_log(NULL, AV_LOG_INFO, "Success to delete file mytest.txt \n");
