520c83117ce4f7ed6ca160dde57b0282baaca58d
Cstzhangzb <devzhangzb@163.com>
2019-04-23 17:22:51 +0800

    add extra_video

--- extra_audio.c
+++ extra_audio.c
@@ -1,19 +1,40 @@
 #include <libavutil/log.h>
 #include <libavformat/avformat.h>
+#include <libavformat/avio.h>
 #include <stdio.h>
 
 #define ADTS_HEADER_LEN 7;
 
-void adts_header(char *szAdtsHeader, int datalen)
+void adts_header(char *adtsHeader, int datalen)
 {
   int audio_object_type = 2;
   int sampling_frequency_index = 7;
   int channel_config = 2;
+
   int adtsLen = datalen + 7;
-  szAdtsHeader[0] = 0xff;
-  szAdtsHeader[1] = 0xf0;
-  szAdtsHeader[1] |= (0 << 3);
-  szAdtsHeader[1] |= (0 << 1);
+
+  adtsHeader[0] = 0xff;      //syncword:0xfff                          高8bits
+  adtsHeader[1] = 0xf0;      //syncword:0xfff                          低4bits
+  adtsHeader[1] |= (0 << 3); //MPEG Version:0 for MPEG-4,1 for MPEG-2  1bit
+  adtsHeader[1] |= (0 << 1); //Layer:0                                 2bits
+  adtsHeader[1] |= 1;        //protection absent:1                     1bit
+
+  adtsHeader[2] = (audio_object_type - 1) << 6;            //profile:audio_object_type - 1                      2bits
+  adtsHeader[2] |= (sampling_frequency_index & 0x0f) << 2; //sampling frequency index:sampling_frequency_index  4bits
+  adtsHeader[2] |= (0 << 1);                               //private bit:0                                      1bit
+  adtsHeader[2] |= (channel_config & 0x04) >> 2;           //channel configuration:channel_config               高1bit
+
+  adtsHeader[3] = (channel_config & 0x03) << 6; //channel configuration:channel_config      低2bits
+  adtsHeader[3] |= (0 << 5);                    //original：0                               1bit
+  adtsHeader[3] |= (0 << 4);                    //home：0                                   1bit
+  adtsHeader[3] |= (0 << 3);                    //copyright id bit：0                       1bit
+  adtsHeader[3] |= (0 << 2);                    //copyright id start：0                     1bit
+  adtsHeader[3] |= ((adtsLen & 0x1800) >> 11);  //frame length：value   高2bits
+
+  adtsHeader[4] = (uint8_t)((adtsLen & 0x7f8) >> 3); //frame length:value    中间8bits
+  adtsHeader[5] = (uint8_t)((adtsLen & 0x7) << 5);   //frame length:value    低3bits
+  adtsHeader[5] |= 0x1f;                             //buffer fullness:0x7ff 高5bits
+  adtsHeader[6] = 0xfc;
 }
 
 int main(int argc, char *argv[])
@@ -69,12 +90,20 @@ int main(int argc, char *argv[])
   audio_index = ret;
   av_init_packet(&pkt);
   //read fream
+  int isFirst = 0;
   while (av_read_frame(fmt_ctx, &pkt) >= 0)
   {
     if (pkt.stream_index == audio_index)
     {
       //TODO
-      // char adts_header_buf[7];
+      if (isFirst == 0)
+      {
+        char adts_header_buf[7];
+        adts_header(adts_header_buf, pkt.size);
+        fwrite(adts_header_buf, 1, 7, dst_fd);
+        isFirst = 1;
+      }
+
       //write file
       len = fwrite(pkt.data, 1, pkt.size, dst_fd);
       if (len != pkt.size)
